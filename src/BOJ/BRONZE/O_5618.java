c1ac3ad97ee8ae6286f777944bf8e07b338b9d0a
ksm7558k <ksm7558k@gmail.com>
2022-03-03 02:04:47 +0900

    알고리즘

---
+++ src/BOJ/BRONZE/O_5618.java
@@ -0,0 +1,38 @@
+package BOJ.BRONZE;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class O_5618 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(bf.readLine());
+        String[] arr = bf.readLine().split(" ");
+        int[] num = new int[n];
+        for (int i = 0; i < n; i++) {
+            num[i] = Integer.parseInt(arr[i]);
+        }
+        Arrays.sort(num);
+
+        int idx = 1;
+        bw.write(1 + "\n");
+        while (idx <= num[0]) {
+            idx++;
+            int hab = 0;
+            for (int i : num) {
+                if (i % idx == 0) {
+                    hab++;
+                }
+            }
+            if (hab == n) {
+                bw.write(idx + "\n");
+            }
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
+
